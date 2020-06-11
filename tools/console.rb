a96ea71f36d22a1f431729f94d7d81d85166c60a
cortezd334 <cortezd334@gmail.com>
2020-06-11 10:45:59 -0700

    CC #2

--- tools/console.rb
+++ tools/console.rb
@@ -6,12 +6,28 @@ end
 
 
 ###  WRITE YOUR TEST CODE HERE ###
+#author
+danira = Author.new("Danira")
+erika = Author.new("Erika")
+david = Author.new("David")
 
+#magazine
+cosmo = Magazine.new("Cosmopolitan", "Fashion")
+cars = Magazine.new("Muscle Cars", "Cars")
+home = Magazine.new("Home Decor", "Home")
+style = Magazine.new("Style", "Fashion")
+cooking = Magazine.new("Home Cooked Meals", "Home")
 
-
-
-
-
+#article
+a1 = Article.new(david, cars, "Fastest Cars")
+a2 = Article.new(david, cars, "When to Upgrade Your Car")
+a3 = Article.new(erika, style, "5 Minute Routine")
+a4 = Article.new(erika, style, "Day to Night")
+a5 = Article.new(erika, cosmo, "Accessories Must Haves")
+a6 = Article.new(danira, home, "Rugs That Tie The Room")
+a7 = Article.new(danira, cooking, "Instacopot Meals")
+a8 = Article.new(danira, cosmo, "How to Make Your Home as Fashionable as You")
+a9 = Article.new(erika, style, "Confidence is the Ultimate Look")
 
 
 
